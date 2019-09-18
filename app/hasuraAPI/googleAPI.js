1de8b681253332dfd8bc130de9a91a7085eaf30d
Jordan Dickerson <fr7739@wayne.edu>
2019-09-18 09:22:59 -0400

    Added in temporary button along with api functionality.

---
+++ app/hasuraAPI/googleAPI.js
@@ -0,0 +1,36 @@
+function googleAPI() 
+{
+var url = "https://auth.charisma19.hasura-app.io/v1/signup";
+
+var requestOptions = {
+    "method": "POST",
+    "headers": {
+        "Content-Type": "application/json"
+    }
+};
+
+var body = {
+    "provider": "google",
+    "data": {
+        "access_token": "oauth-access-token"
+    }
+};
+
+requestOptions.body = JSON.stringify(body);
+
+fetch(url, requestOptions)
+.then(function(response) {
+	return response.json();
+})
+.then(function(result) {
+	console.log(result);
+	// To save the auth token received to offline storage
+	 var authToken = result.auth_token
+	 AsyncStorage.setItem('HASURA_AUTH_TOKEN', authToken);
+})
+.catch(function(error) {
+	console.log('Request Failed:' + error);
+});
+}
+
+export default googleAPI;
--- app/screens/loginForm.js
+++ app/screens/loginForm.js
@@ -3,6 +3,8 @@ import {View, StyleSheet} from 'react-native';
 import {Button} from 'react-native-material-ui';
 import { TextField } from 'react-native-materialui-textfield';
 import loginAPI from '../hasuraAPI/loginAPI';
+import googleAPI from '../hasuraAPI/googleAPI';
+<script src="https://apis.google.com/js/platform.js" async defer></script>
 
 export default class LoginScreen extends React.Component {
 
@@ -68,6 +70,10 @@ export default class LoginScreen extends React.Component {
             <View>
                 <Button style={{ container: styles.buttonStyle}} text="Login" raised={true} primary={true} onPress={ () => loginAPI(this.state.email, this.state.password)}/>
             </View>
+            <View>
+                <Button style={{ container: styles.buttonStyle}} text="Google Login" raised={true} primary={true} onPress={ () => googleAPI()}/>
+            </View>
+
         </View>
         )
     }
