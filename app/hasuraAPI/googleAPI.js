a236b2800917995f76275478c55952fdb7b97949
Jordan Dickerson <fr7739@wayne.edu>
2019-09-25 08:58:50 -0400

    Working facebook and google logins
    Signed-off-by: Jordan Dickerson <fr7739@wayne.edu>

--- app/hasuraAPI/googleAPI.js
+++ app/hasuraAPI/googleAPI.js
@@ -1,8 +1,37 @@
 // Hasura API which creates and logs into an account by authenticating with google
+import * as GoogleSignIn from 'expo-google-sign-in';
+
+initAsync = async () => {
+    await GoogleSignIn.initAsync({
+      clientId: '318411275550-f2ug4n6ht3t7noupl4jkfviumv6ml558.apps.googleusercontent.com',
+    });
+    this._syncUserWithStateAsync();
+  };
+
+  _syncUserWithStateAsync = async () => {
+    const user = await GoogleSignIn.signInSilentlyAsync();
+    this.setState({ user });
+  };
+
+  signOutAsync = async () => {
+    await GoogleSignIn.signOutAsync();
+    this.setState({ user: null });
+  };
+  signInAsync = async () => {
+    try {
+      await GoogleSignIn.askForPlayServicesAsync();
+      const { type, user } = await GoogleSignIn.signInAsync();
+      if (type === 'success') {
+        this._syncUserWithStateAsync();
+      }
+    } catch ({ message }) {
+      alert('login: Error:' + message);
+    }
+  };
 
 function googleAPI() 
 {
-var url = "https://auth.charisma19.hasura-app.io/v1/signup";
+var url = "https://auth.moappetit.com/v1/signup";
 
 var requestOptions = {
     "method": "POST",
